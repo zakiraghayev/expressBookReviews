2ac61bae6486fcefacc4b31b0eb6188f0cb3b8fa
Zakir Aghayev <agayevzakir239@gmail.com>
2023-11-29 07:56:46 -0500

    Task 12 resolved

--- final_project/router/asyncGeneral.js
+++ final_project/router/asyncGeneral.js
@@ -2,17 +2,31 @@ const express = require('express');
 const axios = require('axios');
 const public_users_async = express.Router();
 
-const BASE_URL = "http://localhost:5000/"
+const BASE_URL = "http://localhost:5000"
 // Get the book list available in the shop
 public_users_async.get('/', async function (req, res) {
     //Write your code here
 
     try {
-        const response = await axios.get(BASE_URL);
+        const response = await axios.get(`${BASE_URL}/`);
         return res.status(200).json(response.data);
     } catch (error) {
         return res.status(500).json({ message: error.message });
     }
+});
+
+// Get book details based on ISBN
+public_users_async.get('/isbn/:isbn',async function (req, res) {
+    //Write your code here
+    const { isbn } = req.params;
+
+    try {
+        const response = await axios.get(`${BASE_URL}/isbn/${isbn}`);
+        return res.status(200).json(response.data);
+    } catch (error) {
+        return res.status(500).json({ message: error.message });
+    }
+
 });
 
 module.exports.general = public_users_async;
